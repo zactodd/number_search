fc616192d596ab6e6487c573e7c246bab0c863e8
Zac Todd <zactodd0@gmail.com>
2020-01-20 19:28:03 +1300

    Update number_search.py

--- number_search.py
+++ number_search.py
@@ -3,7 +3,7 @@ DIRECTIONS = {
     "N": (-1, 0),
     "E": (0, 1),
     "S": (1, 0),
-    "W": (-1, 0),
+    "W": (0, -1),
     "NE": (-1, 1),
     "SE": (1, 1),
     "SW": (1, -1),
@@ -12,10 +12,23 @@ DIRECTIONS = {
 
 
 def generate_grid(height, width):
+    """
+    Generates a hxw of arrays of random single digit integers [[ int, ..., int ], ..., [...]].
+    :param height: The height of the grid.
+    :param width: The width of the grid.
+    :return: 
+    """
     return [[random.randint(0, 9) for _ in range(width)] for _ in range(height)]
 
 
 def position_restrictions(word_len, height, width):
+    """
+    GEt the range in which each word can start from given it direction.
+    :param word_len: The length of the words to be place in the grid.
+    :param height: The height of the grid.
+    :param width: The width of the grid.
+    :return: A dictionary that evaluates to direction to the valid placement range.
+    """
     return {
         "N": ((word_len - 1, height - 1), (0, width - 1)),
         "E": ((0, height - 1), (0, width - word_len - 1)),
@@ -29,10 +42,21 @@ def position_restrictions(word_len, height, width):
 
 
 def pprint_grid(grid):
+    """
+    Pretty prints the grid.
+    :param grid: 
+    """
     print("\n".join(" ".join(str(r) for r in g) for g in grid))
 
-
+    
 def generate_words(num_words, word_len, grid):
+    """
+    Generates random words that are located in the specified grid.
+    :param num_words: The number of words to generate.
+    :param word_len: The length of the words to be place in the grid.
+    :param grid: 
+    :return: A list of words to that are located in the grid.
+    """
     height, width = len(grid), len(grid[0])
     restrictions = position_restrictions(word_len, height, width)
     word_hashes = set()
@@ -46,4 +70,9 @@ def generate_words(num_words, word_len, grid):
             words.append("".join(str(grid[x0 + x * i][y0 + y * i]) for i in range(word_len)))
             word_hashes.add(word_hash)
     return sorted(words)
+
+
+
+
+
 
