f82c1f30277ce6eb715f426d9506a9226977dd25
Zac Todd <zactodd0@gmail.com>
2020-01-23 19:35:12 +1300

    Create reciver.py

---
+++ reciver.py
@@ -0,0 +1,47 @@
+import imaplib
+import time
+import json
+
+
+def gmail_matches(uid_search_str, login_key="key.json", username=None, password=None):
+    """
+    Get the unseen emails from given credentials.
+    :param uid_search_str: uid formated search string.
+    :param login_key: a json file containing the username and password information.
+    :param username: the username if not none and :param password is also not none.
+    :param password: the password if not none and :param username is also not none.
+    :return
+    """
+    assert (username is None and password is None) or (username is not None and password is not None), \
+        "Both username and password need to be included otherwise use login json file."
+    with open(login_key, "r") as f:
+        data = json.load(f)
+        username, password = data["username"], data["password"]
+
+    server = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    server.login(username, password)
+    server.select('INBOX')
+    _, data = server.uid('search', None, uid_search_str)
+
+    results = []
+    for s in data[0].split():
+        _, full_data = server.uid('fetch', str(s.decode("utf-8")), '(RFC822)')
+        results.append(full_data)
+    server.logout()
+    return results if len(results) > 0 else None
+
+
+def listen_for_gmail(uid_search_str, login_key="key.json", username=None, password=None, wait=60):
+    """
+    Genertor for looking for emails
+    :param uid_search_str: uid formated search string.
+    :param login_key: a json file containing the username and password information.
+    :param username: the username
+    :param password: the password
+    """
+    while True:
+        emails = gmail_matches(uid_search_str, login_key, username, password)
+        if emails is not None:
+            yield emails
+        time.sleep(wait)
+
