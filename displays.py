ac205388b196de346e28b658dd7c1c424473d4c3
Zac Todd <zactodd0@gmail.com>
2020-02-01 19:51:05 +1300

    Update displays.py

--- displays.py
+++ displays.py
@@ -10,11 +10,11 @@ def number_search_doc(outfile):
     Creates a number search puzzle.
     :param outfile: The file to out the puzzle.
     """
-    height, width, num_words, word_len = 14, 20, 56, 7
+    height, width, num_words, word_len = 14, 20, 33, 7
     grid = generate_grid(height, width)
     formatted_grid = [{"cols": r} for r in grid]
     words = sorted(generate_words(num_words, word_len, grid))
-    rows = num_words // 4
+    rows = num_words // 3
     formatted_words = [{"cols": [words[i + rows * j] for j in range(4)]} for i in range(rows)]
     doc = DocxTemplate("templates/number_search_template.docx")
     doc.render({'grid': formatted_grid,  'search_table': formatted_words})
